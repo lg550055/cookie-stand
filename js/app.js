b21d592fb5f44d852f5c5d28e4e5f1c8c83e391c
Polo <lg550055@hotmail.com>
2021-12-07 15:44:47 -0600

    Adds lab07 functionality: displays hourly sales in table

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ function Store(name, min, max, avg) {
   this.minCust = min;
   this.maxCust = max;
   this.avgPurch = avg;
+  this.eachHour = [];
 }
 
 let seattle = new Store('Seattle',23,65,6.3);
@@ -17,26 +18,52 @@ function randomCount(min, max) {
   return Math.floor(Math.random()*(max-min+1)+min)
 }
 
+function headings() {
+  document.write("<tr>");
+  document.write("<th></th>");
+  for (let i=6; i<20; i++) {
+    if (i<13) {
+      document.write("<th>"+`${i}am`+"</th>");
+    } else {
+      document.write("<th>"+`${i-12}pm`+"</th>");
+    }
+  }
+  document.write("<th>Day total</th>");
+  document.write("</tr>");
+}
+let hour = Array(14).fill(0);
 function hourlySales(store) {
-  document.write("<div>");
-  document.write(store.name);
-  document.write("<br>"+"<br>");
+  document.write("<tr>");
+  document.write("<th>"+store.name+"</th>");
   let n = 0, count = 0;
   for (let i=6; i<20; i++) {
     n = Math.round(store.avgPurch*randomCount(store.minCust,store.maxCust));
     count += n;
-    if (i<13) {
-      document.write(`${i}am: ${n} cookies`+"<br>");
-    } else {
-      document.write(`${i-12}pm: ${n} cookies`+"<br>");
-    }
+    hour[i-6] += n;
+    store.eachHour.push(n);
+    document.write("<td>"+n+"</td>");
   }
-  document.write("<br>"+`Total: ${count} cookies`);
-  document.write("</div>");
+  document.write("<td>"+count+"</td>");
+  document.write("</tr>");
 }
 
+function footings() {
+  document.write("<tfoot>");
+  document.write("<tr>");
+  document.write("<th>Total</th>");
+  for (let i=6; i<20; i++) {
+    document.write("<td>"+hour[i-6]+"</td>");
+  }
+  document.write("<td>"+hour.reduce((a,b)=>a+b)+"</td>");
+  document.write("</tr>");
+  document.write("</tfoot>");
+}
+
+
+headings();
 hourlySales(seattle);
 hourlySales(tokyo);
 hourlySales(dubai);
 hourlySales(paris);
 hourlySales(lima);
+footings();
